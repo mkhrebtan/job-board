f864f9ed5d4cabf2580422fd611a21ab61342b24
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-09-04 13:04:53 +0300

    Add Persistence project reference and update Program.cs
    - Added a project reference to `Persistence.csproj` in `API.csproj` for enhanced functionality.
    - Updated `Program.cs` to include `AddPersistence` for service registration and removed unnecessary OpenAPI comments.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,12 +1,13 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
